7572e18121400d621bd209cef1722857086ea465
Shvec Sergei <fejanin@mail.ru>
2024-01-27 20:13:23 +0300

    Changed settings. Added file requerements.txt.

--- dj_deployment/settings.py
+++ dj_deployment/settings.py
@@ -20,12 +20,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0m+w$-=n8ee44n$8xtw9-x^us^p-_z+^d@z02ddgv=zvp@)o0v'
+# SECRET_KEY = 'django-insecure-0m+w$-=n8ee44n$8xtw9-x^us^p-_z+^d@z02ddgv=zvp@)o0v'
+import os
+
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'fejanin.pythonanywhere.com',
+]
 
 
 # Application definition
@@ -76,8 +87,15 @@ WSGI_APPLICATION = 'dj_deployment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fejanin$default',
+        'USER': 'fejanin',
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': 'fejanin.mysql.pythonanywhere-services.com',
+        'OPTIONS': {
+            'init_command': "SET NAMES 'utf8mb4';SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        }
     }
 }
 
@@ -117,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
